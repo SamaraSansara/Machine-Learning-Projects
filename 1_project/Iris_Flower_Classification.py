b4b33d8273f4eed8754d07b658c4ce8a5eb3f0d3
Veronika <veronika2323.0582@gmail.com>
2025-11-02 23:39:24 +0300

    Update Iris_Flower_Classification.py

--- 1_project/Iris_Flower_Classification.py
+++ 1_project/Iris_Flower_Classification.py
@@ -9,27 +9,44 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 import matplotlib
 matplotlib.use('Agg')
 
-iris = load_iris()
+
+iris = load_iris() #Загрузка датасета
+
+
+# Преобразование данных в DataFrame
 df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 df['species'] = iris.target
+# Мы преобразуем массив данных из iris.data в таблицу (DataFrame) с помощью pandas.
+# Добавляем новый столбец species, который содержит целевые метки (классы цветков).
 
-print(df.head())
+print(df.head()) # Эта команда выводит первые 5 строк таблицы
 
-X = df.drop('species', axis=1)
-y = df['species']
 
+# Разделение данных на признаки и целевую переменную
+X = df.drop('species', axis=1) # Матрица признаков (все столбцы, кроме species).
+y = df['species'] # Матрица признаков (все столбцы, кроме species).
+
+
+# Разделение данных на обучающую и тестовую выборки
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Обучающая выборка (X_train, y_train) — 80% данных.
+# Тестовая выборка (X_test, y_test) — 20% данных.
 
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
 
+scaler = StandardScaler() # нормализует данные, приводя их к стандартному виду (среднее = 0, стандартное отклонение = 1).
+X_train = scaler.fit_transform(X_train) # Применяется к обучающей выборке для обучения масштабировщика.
+X_test = scaler.transform(X_test) # Применяется к тестовой выборке для использования уже обученного масштабировщика.
+
+
+
+# Мы создаём модель KNN с параметром n_neighbors=3 (используется 3 ближайших соседа для классификации).
+# Обучаем модель на обучающих данных (X_train, y_train).
 knn = KNeighborsClassifier(n_neighbors=3)
 
 knn.fit(X_train, y_train)
-y_pred = knn.predict(X_test)
+y_pred = knn.predict(X_test)  # Модель предсказывает классы для тестовых данных (X_test). Результат хранится в y_pred.
 
-accuracy = accuracy_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred) #  Сравнивает истинные значения (y_test) с предсказанными (y_pred) и вычисляет точность.
 print(f"Точность модели: {accuracy * 100:.2f}%")
 print("Отчет по классификации:")
 print(classification_report(y_test, y_pred))
@@ -42,6 +59,3 @@ plt.xlabel('Длина лепестка (см)')
 plt.ylabel('Ширина лепестка (см)')
 plt.title('Распределение видов ирисов')
 plt.savefig("iris_scatter.png")
-
-
-
